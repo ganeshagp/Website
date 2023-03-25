9ac2dfe3edc3d6315072bd400bf2d03123765f74
Yugendran <yugendran.sunder@gmail.com>
2023-03-25 22:18:06 +0530

    Update __init__.py

--- flaskpage/__init__.py
+++ flaskpage/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-import pymongo
                 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
